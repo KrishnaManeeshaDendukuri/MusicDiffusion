32247c45d9429c00759c8bc626295e1d022a10ab
Faeze Pouyamehr <faezepooyaofficial@gmail.com>
2022-11-26 11:25:16 -0500

    Update main.py

--- Mid2spectrogram/main.py
+++ Mid2spectrogram/main.py
@@ -1,7 +1,7 @@
 from midi2audio import FluidSynth
 import matplotlib.pyplot as plt
 import scipy.io.wavfile as wavfile
-
+# see this page: https://ksvi.mff.cuni.cz/~dingle/2019/prog_1/python_music.html to get "FluidR3_GM.sf2"
 fs = FluidSynth('FluidR3_GM.sf2')
 fs.midi_to_audio('input.mid', 'output1.wav')
 
